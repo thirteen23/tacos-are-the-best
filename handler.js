880d8bf3b49bfe4bec6a204534c26a14190691e2
Tom Hudson <tomhudson@130-Toms-Comp.local>
2021-08-05 09:35:53 -0500

    Added _ and better names to constants

--- handler.js
+++ handler.js
@@ -1,8 +1,9 @@
 "use strict";
 const AWS = require("aws-sdk");
+const S3 = new AWS.S3();
 
 module.exports.tacos = async (event) => {
-  const QUERYOBJ = {
+  const TACO_DESCRIPTION = {
     type: getParameter(event, "type"),
     tortilla: getParameter(event, "tortilla"),
     filling1: getParameter(event, "filling1"),
@@ -14,15 +15,36 @@ module.exports.tacos = async (event) => {
   const data =
     process.env.LOCAL_DEV === "true"
       ? require(`./data/tacos.json`)
-      : null;
+      : null; 
   
-  const TACOCOUNT = filterTacos(data, QUERYOBJ)
+  const bucket = "tacos-are-the-best";
+  const key = `tacos.json`;
+  const params = {
+    Bucket: bucket,
+    Key: key,
+  };
+
+  let tacoData;
+  if (process.env.LOCAL_DEV === "true") {
+    tacoData = data;
+  } else {
+    let fileData;
+    try {
+      fileData = await S3.getObject(params).promise();
+    } catch (error) {
+      console.log(error);
+    }
+    let fileContents = fileData.Body.toString("utf-8");
+    tacoData = JSON.parse(fileContents);
+  }
+  
+  const TACO_COUNT = filterTacos(tacoData, TACO_DESCRIPTION)
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: `You have matched ${TACOCOUNT} tacos!`,
+        message: `You have matched ${TACO_COUNT} tacos!`,
         input: event,
       },
       null,
@@ -39,14 +61,14 @@ function getParameter(event, item) {
   }
 }
 
-function filterTacos(data, queryObj) {
+function filterTacos(data, TACO_DESCRIPTION) {
   const TACOS = data.collection;
   return TACOS.filter(taco => 
-    taco.type === queryObj.type && 
-    taco.tortilla === queryObj.tortilla &&
-    taco.filling1 === queryObj.filling1 &&
-    taco.filling2 === queryObj.filling2 &&
-    taco.filling3 === queryObj.filling3 &&
-    taco.salsa === queryObj.salsa
+    taco.type === TACO_DESCRIPTION.type && 
+    taco.tortilla === TACO_DESCRIPTION.tortilla &&
+    taco.filling1 === TACO_DESCRIPTION.filling1 &&
+    taco.filling2 === TACO_DESCRIPTION.filling2 &&
+    taco.filling3 === TACO_DESCRIPTION.filling3 &&
+    taco.salsa === TACO_DESCRIPTION.salsa
   ).length;
 }
